21d0ef12d0191f421b4a3e2d43688ad12f2943bd
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2021-08-14 16:12:12 -0300

    Inclusão do projeto eureka-server

--- pessoa-ws/src/main/java/br/com/tech4me/pessoaws/PessoaWsApplication.java
+++ pessoa-ws/src/main/java/br/com/tech4me/pessoaws/PessoaWsApplication.java
@@ -2,8 +2,10 @@ package br.com.tech4me.pessoaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PessoaWsApplication {
 
 	public static void main(String[] args) {
